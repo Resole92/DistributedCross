dfce26ec3528682a8673e177040559e21b3f2f68
darko <dellapietradarko@yahoo.it>
2022-05-03 18:09:34 +0200

    Check all storyboard

--- Distributed.Cross.Common/Distributed.Cross.Gui.Simulation/Environment/EnvironmentViewModel.cs
+++ Distributed.Cross.Common/Distributed.Cross.Gui.Simulation/Environment/EnvironmentViewModel.cs
@@ -20,6 +20,28 @@ namespace Distributed.Cross.Gui.Simulation.Environment
         private static EnvironmentViewModel _instance;
         public static EnvironmentViewModel Instance => _instance ??= new EnvironmentViewModel();
 
+        private int _vehicle1Destination = 0;
+        public int Vehicle1Destination
+        {
+            get => _vehicle1Destination;
+            set
+            {
+                _vehicle1Destination = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int _vehicle2Destination = 0;
+        public int Vehicle2Destination
+        {
+            get => _vehicle2Destination;
+            set
+            {
+                _vehicle2Destination = value;
+                NotifyPropertyChanged();
+            }
+        }
+
         private int _vehicle3Destination = 0;
         public int Vehicle3Destination
         {
@@ -31,14 +53,52 @@ namespace Distributed.Cross.Gui.Simulation.Environment
             }
         }
 
-        public RelayCommand DestinationLane5Command =>
+        private int _vehicle4Destination = 0;
+        public int Vehicle4Destination
+        {
+            get => _vehicle4Destination;
+            set
+            {
+                _vehicle4Destination = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public RelayCommand Destination1Lane5Command =>
+            new RelayCommand(_ => Vehicle1Destination = 5);
+        public RelayCommand Destination1Lane6Command =>
+            new RelayCommand(_ => Vehicle1Destination = 6);
+        public RelayCommand Destination1Lane7Command =>
+            new RelayCommand(_ => Vehicle1Destination = 7);
+        public RelayCommand Destination1Lane8Command =>
+            new RelayCommand(_ => Vehicle1Destination = 8);
+
+        public RelayCommand Destination2Lane5Command =>
+            new RelayCommand(_ => Vehicle2Destination = 5);
+        public RelayCommand Destination2Lane6Command =>
+            new RelayCommand(_ => Vehicle2Destination = 6);
+        public RelayCommand Destination2Lane7Command =>
+            new RelayCommand(_ => Vehicle2Destination = 7);
+        public RelayCommand Destination2Lane8Command =>
+            new RelayCommand(_ => Vehicle2Destination = 8);
+
+        public RelayCommand Destination3Lane5Command =>
             new RelayCommand(_ => Vehicle3Destination = 5);
-        public RelayCommand DestinationLane6Command =>
+        public RelayCommand Destination3Lane6Command =>
             new RelayCommand(_ => Vehicle3Destination = 6);
-        public RelayCommand DestinationLane7Command =>
+        public RelayCommand Destination3Lane7Command =>
             new RelayCommand(_ => Vehicle3Destination = 7);
-        public RelayCommand DestinationLane8Command =>
+        public RelayCommand Destination3Lane8Command =>
             new RelayCommand(_ => Vehicle3Destination = 8);
+
+        public RelayCommand Destination4Lane5Command =>
+            new RelayCommand(_ => Vehicle4Destination = 5);
+        public RelayCommand Destination4Lane6Command =>
+            new RelayCommand(_ => Vehicle4Destination = 6);
+        public RelayCommand Destination4Lane7Command =>
+            new RelayCommand(_ => Vehicle4Destination = 7);
+        public RelayCommand Destination4Lane8Command =>
+            new RelayCommand(_ => Vehicle4Destination = 8);
 
 
         public RelayCommand StartEnvironmentCommand
